27958a3a9568e34f3ac55bb6245f8b831c4dc56e
bvishal8510 <baghel.vishal124124@gmail.com>
2024-09-10 08:45:45 +0530

    some more patterns

--- patterns.js
+++ patterns.js
@@ -1,3 +1,6 @@
+// https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
+
+
 // function printPattern(n) {
 //     let str = "";
 //     for(let i=0;i<n;++i) {
@@ -63,15 +66,73 @@
 
 // printPattern(7)
 
+// function printPattern(n) {
+//     let str = "";
+//     for(let i=n;i>0;--i) {
+//         str="";
+//         for(let j=0;j<i;++j) {
+//             str += String(j+1)+" ";
+//         }
+//         console.log(str);
+//     }
+// }
+
+// printPattern(7)
+
+// function printPattern(n) {
+//     for(let z=0;z<n;++z) {
+//         let str = "";
+//         for(let i=n-z-1;i>0;--i) str += " ";
+//         for(let j=0;j<z+1;j++) str += "*";
+//         for(let k=0;k<z;++k) str += "*";
+//         console.log(str);
+//     }
+// }
+
+// printPattern(7)
+
+// function printPattern(n) {
+//     for(let z=0;z<n;++z) {
+//         let str = "";
+//         for(let i=0;i<z;++i) str += " ";
+//         for(let j=n-z;j>0;--j) str += "*";
+//         for(let k=n-z-1;k>0;--k) str += "*";
+//         console.log(str);
+//     }
+// }
+
+// printPattern(7)
+
+// function printPattern(n) {
+//     for(let z=0;z<n;++z) {
+//         let str = "";
+//         for(let i=n-z-1;i>0;--i) str += " ";
+//         for(let j=0;j<z+1;j++) str += "*";
+//         for(let k=0;k<z;++k) str += "*";
+//         console.log(str);
+//     }
+//     for(let z=0;z<n;++z) {
+//         let str = "";
+//         for(let i=0;i<z;++i) str += " ";
+//         for(let j=n-z;j>0;--j) str += "*";
+//         for(let k=n-z-1;k>0;--k) str += "*";
+//         console.log(str);
+//     }
+// }
+
+// printPattern(7)
+
 function printPattern(n) {
-    let str = "";
-    for(let i=n;i>0;--i) {
-        str="";
-        for(let j=0;j<i;++j) {
-            str += String(j+1)+" ";
-        }
+    for(let z=0;z<n;++z) {
+        let str="";
+        for(let i=0;i<z+1;++i) str += "*";
+        console.log(str);
+    }
+    for(let z=0;z<n-1;++z) {
+        let str="";
+        for(let i=n-z-1;i>0;--i) str += "*";
         console.log(str);
     }
 }
 
-printPattern(7)
+printPattern(5)
