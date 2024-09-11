5bb82a8b35bffa0768a14943897730abdce4a13b
bvishal8510 <baghel.vishal124124@gmail.com>
2024-09-11 08:44:21 +0530

    some more patterns

--- patterns.js
+++ patterns.js
@@ -122,17 +122,46 @@
 
 // printPattern(7)
 
+// function printPattern(n) {
+//     for(let z=0;z<n;++z) {
+//         let str="";
+//         for(let i=0;i<z+1;++i) str += "*";
+//         console.log(str);
+//     }
+//     for(let z=0;z<n-1;++z) {
+//         let str="";
+//         for(let i=n-z-1;i>0;--i) str += "*";
+//         console.log(str);
+//     }
+// }
+
+// printPattern(5)
+
+
+// function printPattern(n) {
+//     let s;
+//     for(let i=0;i<n;++i) {
+//         if(i%2===0) s=1;
+//         else s=0;
+//         let str = "";
+//         for(let j=0;j<i+1;++j) {
+//             str += s + " ";
+//             s = 1-s;
+//         }
+//         console.log(str);
+//     }
+// }
+
+// printPattern(6)
+
 function printPattern(n) {
     for(let z=0;z<n;++z) {
-        let str="";
-        for(let i=0;i<z+1;++i) str += "*";
-        console.log(str);
-    }
-    for(let z=0;z<n-1;++z) {
-        let str="";
-        for(let i=n-z-1;i>0;--i) str += "*";
+        let str = "";
+        for(let i=0;i<z+1;++i) str += i+1;
+        for(let j=0;j<(n-z-2)*2;++j) str += " ";
+        for(let k=0;k<z+1;++k) str += k+1;
         console.log(str);
     }
 }
 
-printPattern(5)
+printPattern(6)
