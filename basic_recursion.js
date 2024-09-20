a7a5aef39091dbb64e30af811dd4ac50430ef229
bvishal8510 <baghel.vishal124124@gmail.com>
2024-09-20 08:37:13 +0530

    some basic recursion

--- basic_recursion.js
+++ basic_recursion.js
@@ -15,11 +15,14 @@
 // print1ToN(7);
 
 // // https://takeuforward.org/data-structure/sum-of-first-n-natural-numbers/
-function sumOfFirstNNumbers(n) {
-    if(n===1) return 1;
-    return (n + sumOfFirstNNumbers(n-1));
-}
-console.log(sumOfFirstNNumbers(100000));
+// function sumOfFirstNNumbers(n) {
+//     // if(n===1) return 1;
+//     // return (n + sumOfFirstNNumbers(n-1));
+
+//     let sum = BigInt((n*(n+1))/2);
+//     return String(sum * sum);
+// }
+// console.log(sumOfFirstNNumbers(18456));
 
 // // https://takeuforward.org/data-structure/factorial-of-a-number-iterative-and-recursive/
 // function factorial(n) {
@@ -28,3 +31,28 @@ console.log(sumOfFirstNNumbers(100000));
 // }
 // console.log(factorial(5));
 
+// // https://takeuforward.org/data-structure/reverse-a-given-array/
+// function reverseArray(arr, i=0) {
+//     if (i === (arr.length)) {
+//         return [];
+//     }
+//     let result = reverseArray(arr, i+1);
+//     result.push(arr[i]);
+//     return result;
+// }
+// console.log(reverseArray([1]))
+
+function reverseArray(arr) {
+    let i=0;
+    let l = arr.length-1;
+
+    while(i<l) {
+        let temp = arr[i];
+        arr[i]=arr[l];
+        arr[l]=temp;
+        ++i;
+        --l;
+    }
+    return arr;
+}
+console.log(reverseArray([1,2,3,4]))
