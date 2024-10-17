baa3e15ca79315c11c837eda5065e2852f0297f1
bvishal8510 <baghel.vishal124124@gmail.com>
2024-10-17 08:36:58 +0530

    rotate array by 1

--- arrays.js
+++ arrays.js
@@ -1,9 +1,7 @@
 // // https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-element-in-array
 // function largestElement(arr) {
 //     let largest = arr[0];
-
 //     for(let i=1;i<arr.length;++i) if(arr[i]>largest) largest=arr[i];
-
 //     return largest;
 // }
 // console.log(largestElement([1, 8, 7, 56, 90]))
@@ -22,7 +20,6 @@
 //         l=arr[1];
 //         sl=arr[0];
 //     }
-
 //     for(let i=2;i<arr.length;++i) {
 //         if (arr[i]>l) {
 //             if(l>sl) sl=l;
@@ -32,28 +29,54 @@
 //             if (arr[i]>sl) sl=arr[i];
 //         }
 //     }
-
 //     return sl;
 // }
 // console.log(secondlargestElement([1, 8, 7, 56, 90]))
 
-// optimized
-function secondlargestElement(arr) {
-    if(arr.length < 2) return -1;
+// // optimized
+// function secondlargestElement(arr) {
+//     if(arr.length < 2) return -1;
+//     let l=-1;
+//     let sl=-1;
+//     for(let i=0;i<arr.length;++i) {
+//         if(arr[i]>l) {
+//             sl=l;
+//             l=arr[i]
+//         }
+//         else if ((arr[i]>sl)&&(arr[i]!==l)) {
+//             sl=arr[i];
+//         }
+//     }
+//     return sl;
+// }
+// console.log(secondlargestElement([10,1,10]))
 
-    let l=-1;
-    let sl=-1;
+// // https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/submissions/1408828746/
+// function arrayRotatedAndSorted(arr) {
+//     let count = 0;
+//     for(let i=0;i<arr.length;++i) {
+//         if (arr[i]>arr[(i+1)%arr.length]) {
+//             if(count<1) ++count;
+//             else return false;
+//         } 
+//     }
+//     return true;
+// }
+// console.log(arrayRotatedAndSorted([5,4,6,7,8]))
 
-    for(let i=0;i<arr.length;++i) {
-        if(arr[i]>l) {
-            sl=l;
-            l=arr[i]
-        }
-        else if ((arr[i]>sl)&&(arr[i]!==l)) {
-            sl=arr[i];
-        }
-    }
-    return sl;
-}
-
-console.log(secondlargestElement([10,1,10]))
+// // https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1408842737/
+// function removeDuplicatesFromSorted(nums) {
+//     let i=1,j=1;
+//     while(i<nums.length) {
+//         if(nums[i]!==nums[i-1]) {
+//             nums[j]=nums[i];
+//             ++j;
+//         }
+//         ++i;
+//     }
+//     while(nums.length>j) {
+//         nums.pop();
+//     }
+//     return nums;
+// }
+// console.log(removeDuplicatesFromSorted([1,1]))
--- sorting.js
+++ sorting.js
@@ -12,26 +12,65 @@
 // }
 // console.log(bubbleSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 10))
 
-function selectionSort(arr, n) {
-    let m;
-    let mp;
+// function selectionSort(arr, n) {
+//     let m;
+//     let mp;
+//     for(let i=0;i<n-1;++i) {
+//         m = arr[i];
+//         mp = i;
+//         for(let j=i+1;j<n;++j) {
+//             if(arr[j]<m) {
+//                 m=arr[j];
+//                 mp=j;
+//             }
+//         }
+//         if (mp !== i) {
+//             let temp = arr[i];
+//             arr[i] = m;
+//             arr[mp] = temp;
+//         }
+//     }
+//     return arr;
+// }
+// console.log(selectionSort([10], 1))
 
-    for(let i=0;i<n-1;++i) {
-        m = arr[i];
-        mp = i;
-        for(let j=i+1;j<n;++j) {
-            if(arr[j]<m) {
-                m=arr[j];
-                mp=j;
-            }
-        }
+// https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+// function sort012(arr) {
+//     let s=0;
+//     let l=arr.length-1;
+//     let i=0;
+//     let temp;
+//     while(i<=l) {
+//         if(arr[i]===0) {
+//             if(i!==s) {
+//                 temp=arr[i];
+//                 arr[i]=arr[s];
+//                 arr[s]=temp;
+//             }
+//             ++s;
+//         }
+//         else if(arr[i]===2) {
+//             if(i!==l) {
+//                 temp=arr[i];
+//                 arr[i]=arr[l];
+//                 arr[l]=temp;
+//             }
+//             --l;
+//         }
+//         ++i;
+//     }
+//     return arr;
+// }
+// console.log(sort012([1,0, 2, 1, 1, 1, 0]));
 
-        if (mp !== i) {
-            let temp = arr[i];
-            arr[i] = m;
-            arr[mp] = temp;
-        }
+// https://leetcode.com/problems/rotate-array/description/
+function rotateArray(arr) {
+    let temp=arr[0];
+    for(let i=1;i<arr.length;++i) {
+        arr[i-1]=arr[i];
     }
+    arr[arr.length-1]=temp;
     return arr;
 }
-console.log(selectionSort([10], 1))
+
+console.log(rotateArray([1,2,3,4,5]))
